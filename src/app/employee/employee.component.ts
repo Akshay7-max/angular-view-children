d51f6584bb890b5f9813cbe08a044671d5f64938
Akshay Kumbhare <52664007+Akshay7-max@users.noreply.github.com>
2022-10-06 14:42:00 +0530

    Update employee.component.ts

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-employee',
@@ -7,8 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  @Input() x!:string;
-
+  empname:string = "Shyam";
   constructor() { }
 
   ngOnInit(): void {
