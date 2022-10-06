1b086d802f6374593ad13a27f9fee0f94d2b2550
Akshay Kumbhare <52664007+Akshay7-max@users.noreply.github.com>
2022-10-06 14:40:46 +0530

    Update company.component.ts

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChildren, QueryList } from '@angular/core';
+import {EmployeeComponent} from "../employee/employee.component";
 
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
   styleUrls: ['./company.component.css']
 })
-export class CompanyComponent implements OnInit {
+export class CompanyComponent {
 
-  phonenumber:string = "8999798993";
+  companyname: string = "ABC Company";
+  
+  @ViewChildren(EmployeeComponent) emp!:QueryList<EmployeeComponent>;
+
+  onClickMeClicked() {
+    console.log(this.emp);
+    var a = this.emp.toArray();
+    for(var i=0; i<a.length; i++){
+      a[i].empname = "Ram";
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
